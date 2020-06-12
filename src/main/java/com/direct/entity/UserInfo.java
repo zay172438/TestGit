9276957f924b583d2a80e528ca6efdccdef6ee90
tom_glb <goodMorning_glb@zx.com>
2020-06-12 10:47:48 +0800

    hot_fix commit

--- src/main/java/com/direct/entity/UserInfo.java
+++ src/main/java/com/direct/entity/UserInfo.java
@@ -1,5 +1,5 @@
 package com.direct.entity;
 
 public class UserInfo {
-
+	private int uid;
 }
--- src/test/java/com/direct/test/TestMain.java
+++ src/test/java/com/direct/test/TestMain.java
@@ -4,5 +4,6 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("left....");
 		System.out.println("right.....");
+		System.out.println("hot_fix");
 	}
 }
